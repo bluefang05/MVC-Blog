82e42086e9472d3a7053b0f843f0d2008ab21ab5
Enmanuel Dominguez <bluefang05@gmail.com>
2020-07-16 23:53:18 -0400

    start

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _ctx;
+
+        public HomeController(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }  
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            _ctx.Post.Add(post);
+            await _ctx.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
